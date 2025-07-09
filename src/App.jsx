3fa1a50ddfd7f43920f9bbfaace006fcbc9db14a
Nitanjr <nittanjr11@gmail.com>
2025-07-09 11:08:45 +0545

    Update: WIP

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import CourseForm from "./components/CourseForm";
+import CourseList from "./components/CourseList";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +15,7 @@ function App() {
       </h1>
 
       <CourseForm />
+      <CourseList />
     </div>
   );
 }
